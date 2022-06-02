948bb2e01ab43ce9e579e862d5722db6bf7b6031
thuveepan <tthuvee@gmail.com>
2022-06-02 08:55:06 +0530

    basket microservices

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Entites;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController: ControllerBase
+    {
+        private readonly IBasketRepository _basket;
+
+        public BasketController(IBasketRepository basket)
+        {
+            _basket = basket;
+        }
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var baseket = await _basket.GetBasket(userName);
+            return Ok(baseket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart shoppingCart)
+        {
+           
+                var basket = await _basket.UpdateBasket(shoppingCart);
+                return Ok(basket);
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteBasket(string userName)
+        {
+            await _basket.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
--- src/Services/Catelog/Catelog.API/Data/CatelogContext.cs
+++ src/Services/Catelog/Catelog.API/Data/CatelogContext.cs
@@ -8,6 +8,7 @@ namespace Catelog.API.Data
     {
         public CatelogContext(IConfiguration configuration)
         {
+
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
